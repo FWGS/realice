d2952e54410afb3df3b8599382977ea5cbbcea35
Alibek Omarov <a1ba.omarov@gmail.com>
2026-02-08 08:52:46 +0500

    Get rid of remainings of VM interpreter

--- code/qcommon/vm.c
+++ code/qcommon/vm.c
@@ -532,8 +532,7 @@ int	QDECL VM_Call( vm_t *vm, int callnum, ... ) {
 	int args[16];
 	va_list ap;
 
-
-	if ( !vm ) {
+	if ( !vm || !vm->entryPoint ) {
 		Com_Error( ERR_FATAL, "VM_Call with NULL vm" );
 	}
 
@@ -542,30 +541,24 @@ int	QDECL VM_Call( vm_t *vm, int callnum, ... ) {
 	lastVM = vm;
 
 	if ( vm_debugLevel ) {
-	  Com_Printf( "VM_Call( %i )\n", callnum );
+		Com_Printf( "VM_Call( %i )\n", callnum );
 	}
 
 	// if we have a dll loaded, call it directly
-	if ( vm->entryPoint ) {
-		//rcg010207 -  see dissertation at top of VM_DllSyscall() in this file.
-		va_start(ap, callnum);
-		for (i = 0; i < sizeof (args) / sizeof (args[i]); i++) {
-			args[i] = va_arg(ap, int);
-		}
-		va_end(ap);
-
-		r = vm->entryPoint( callnum,  args[0],  args[1],  args[2], args[3],
-                            args[4],  args[5],  args[6], args[7],
-                            args[8],  args[9], args[10], args[11],
-                            args[12], args[13], args[14], args[15]);
-	} else if ( vm->compiled ) {
-		r = VM_CallCompiled( vm, &callnum );
-	} else {
-		r = VM_CallInterpreted( vm, &callnum );
+	//rcg010207 -  see dissertation at top of VM_DllSyscall() in this file.
+	va_start(ap, callnum);
+	for (i = 0; i < sizeof (args) / sizeof (args[i]); i++) {
+		args[i] = va_arg(ap, int);
 	}
+	va_end(ap);
+
+	r = vm->entryPoint( callnum,  args[0],  args[1],  args[2], args[3],
+		args[4],  args[5],  args[6], args[7],
+		args[8],  args[9], args[10], args[11],
+		args[12], args[13], args[14], args[15]);
 
 	if ( oldVM != NULL ) // bk001220 - assert(currentVM!=NULL) for oldVM==NULL
-	  currentVM = oldVM;
+		currentVM = oldVM;
 	return r;
 }
 
@@ -683,13 +676,3 @@ void VM_LogSyscalls( int *args ) {
 	fprintf(f, "%i: %i (%i) = %i %i %i %i\n", callnum, args - (int *)currentVM->dataBase,
 		args[0], args[1], args[2], args[3], args[4] );
 }
-
-
-
-#ifdef DLL_ONLY // bk010215 - for DLL_ONLY dedicated servers/builds w/o VM
-int	VM_CallCompiled( vm_t *vm, int *args ) {
-  return(0); 
-}
-
-void VM_Compile( vm_t *vm, vmHeader_t *header ) {}
-#endif // DLL_ONLY
